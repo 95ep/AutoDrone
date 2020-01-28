4a7dfc8540d48d6597113f38a596263887c20efc
Erik Persson <erik.ao.persson@gmail.com>
2020-01-28 14:10:42 +0100

    Some updates to testMovingDrone

--- Point2Point/testMovingDrone.py
+++ Point2Point/testMovingDrone.py
@@ -11,27 +11,47 @@ client.armDisarm(True)
 client.takeoffAsync().join()
 
 # Start at origin
-print("Move to origin")
-client.moveToPositionAsync(0,0,0,3)
+client.moveToPositionAsync(0,0,0,10).join()
 
-# Rotate 30 deg left
-print("Rotate left")
-client.rotateYawAsync(-30).join()
+
+airsim.wait_key('Press any key to move forward')
+selfPos = client.simGetGroundTruthKinematics().position
+
+
+
+airsim.wait_key('Press any key to go down')
+client.moveToPositionAsync(0,0,10,3).join()
 
 # Move forward
-print("Move forward")
+airsim.wait_key('Press any key to move forward')
 client.moveByVelocityAsync(1, 0, 0, 3).join()
+print('Hovering')
+client.hoverAsync().join()
+
+airsim.wait_key('Press any key test rotation')
+
+print('Rotate to 30 deg using YawMode')
+client.moveByVelocityAsync(0, 0, 0, 5, yaw_mode=airsim.YawMode(False, 30)).join()
+
+
+print('Rotate to 120 deg')
+client.moveByVelocityAsync(0, 0, 0, 5, yaw_mode=airsim.YawMode(False, 120)).join()
+
+
+print('Rotate to 0 deg')
+client.moveByVelocityAsync(0, 0, 0, 5, yaw_mode=airsim.YawMode(False, 0)).join()
+
+print('Rotate to 160 deg')
+client.moveByVelocityAsync(0, 0, 0, 0, yaw_mode=airsim.YawMode(False, 160)).join()
 
 # Move up
-print("Move up")
-client.moveByVelocityAsync(0, 0, 1, 3).join()
-
-# Rotate 30 deg right
-print("Rotate right")
-client.rotateYawAsynd(30).join()
+airsim.wait_key('Press any key to move up')
+client.moveByVelocityAsync(0, 0, -1, 2).join()
+client.hoverAsync().join()
 
 
+airsim.wait_key('Press any key to land and disconnect')
 # Disarm, reset and disconnect
-client.armDisarm()
-client.reset()
+client.landAsync().join()
+client.armDisarm(False)
 client.enableApiControl(False)
