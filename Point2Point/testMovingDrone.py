Bug fix regarding mixing up radian and deg when calculating vel vector

Point2Point/testMovingDrone.py
@@ -4,47 +4,47 @@ import time
 
 
 def quaternion2Yaw(q):
-    # Convert from quaternion to Yaw (according to Wikipedia)
+    # Convert from quaternion to Yaw in radians (according to Wikipedia)
     siny_cosp = 2 * (q.w_val * q.z_val + q.x_val * q.y_val)
     cosy_cosp = 1 - 2 * (q.y_val * q.y_val + q.z_val * q.z_val)
-    yaw = np.arctan2(siny_cosp, cosy_cosp) /(2*np.pi)*360
+    yaw = np.arctan2(siny_cosp, cosy_cosp)
     return yaw
 
 
-def rotateLeft(client):
+def rotateLeft(currentClient):
     # Rotate UAV approx 30 deg to the left (counter-clockwise)
-    client.rotateByYawRateAsync(-30, 1).join()
+    currentClient.rotateByYawRateAsync(-30, 1).join()
     # Stop rotation
-    client.rotateByYawRateAsync(0, 1e-6).join()
+    currentClient.rotateByYawRateAsync(0, 1e-6).join()
 
 
-def rotateRight(client):
+def rotateRight(currentClient):
     # Rotate UAV approx 30 deg to the right (clockwise)
-    client.rotateByYawRateAsync(30,1).join()
+    currentClient.rotateByYawRateAsync(30, 1).join()
     # Stop rotation
-    client.rotateByYawRateAsync(0, 1e-6).join()
+    currentClient.rotateByYawRateAsync(0, 1e-6).join()
 
 
-def moveForward(client):
-    q = client.simGetGroundTruthKinematics().orientation
+def moveForward(currentClient):
+    q = currentClient.simGetGroundTruthKinematics().orientation
     yaw = quaternion2Yaw(q)
     # Calc velocity vector of magnitude 1 in direction of UAV
     vel = (np.cos(yaw), np.sin(yaw), 0) # Keep z fixed for now
     # Move forward approx 1 meter
-    client.moveByVelocityAsync(vel[0], vel[1], vel[2], duration=1).join()
+    currentClient.moveByVelocityAsync(vel[0], vel[1], vel[2], duration=1).join()
     # Stop the UAV
-    client.moveByVelocityAsync(0,0,0, duration=1e-6).join()
+    currentClient.moveByVelocityAsync(0, 0, 0, duration=1e-6).join()
 
     # Debugging message
-    print('Calculated yaw is {} deg'.format(yaw))
+    print('Calculated yaw is {} deg'.format(yaw/np.pi*180))
 
 
-def printInfo(client):
-    pos = client.simGetGroundTruthKinematics().position
-    q = client.simGetGroundTruthKinematics().orientation
-    yaw = quaternion2Yaw(q)
+def printInfo(currentClient):
+    pos = currentClient.simGetGroundTruthKinematics().position
+    q = currentClient.simGetGroundTruthKinematics().orientation
+    yawDeg = quaternion2Yaw(q)/np.pi*180
 
-    print('Current yaw is {} deg'.format(yaw))
+    print('Current yaw is {} deg'.format(yawDeg))
     print('Current position is ({}, {}, {})'.format(pos.x_val, pos.y_val, pos.z_val))
 
 