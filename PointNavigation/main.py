df7d6ec6b9279754b3bfc8fb302abe1867cd2054
Filip Heikkilä <35920784+filhei@users.noreply.github.com>
2020-01-30 10:14:31 +0100

    Manual update

--- PointNavigation/main.py
+++ PointNavigation/main.py
@@ -8,8 +8,11 @@ from gym import spaces
 
 import sys
 # insert at 1, 0 is the script path (or '' in REPL)
-sys.path.insert(1, 'C:/Users/Filip/Projects/RISE/AutoDrone/PointNavigation/habitat_baselines')
-sys.path.insert(1, 'C:/Users/Filip/Projects/RISE/AutoDrone/PointNavigation')
+# sys.path.insert(1, 'C:/Users/Filip/Projects/RISE/AutoDrone/PointNavigation/habitat_baselines')
+# sys.path.insert(1, 'C:/Users/Filip/Projects/RISE/AutoDrone/PointNavigation')
+
+sys.path.insert(1, 'C:/Users/Filip/Documents/Skola/Exjobb/AutoDrone/PointNavigation/habitat_baselines')
+sys.path.insert(1, 'C:/Users/Filip/Documents/Skola/Exjobb/AutoDrone/PointNavigation')
 
 from habitat_baselines.rl.ddppo.policy.resnet_policy import PointNavResNetPolicy
 
@@ -32,7 +35,7 @@ actor_critic = PointNavResNetPolicy(observation_space, action_space)
 #weight_file = 'gibson-4plus-resnet50.pth' # depth only
 #weight = 'gibson-2plus-se-resneXt50-rgb.pth'
 #weight_path = weight_directory + weight_file
-weight_path = 'habitat_baselines/ddppo-models/gibson-4plus-resnet50.pth'
+weight_path = 'habitat_baselines/ddppo-models/gibson-2plus-resnet50.pth'
 weight_dict = torch.load(weight_path)["state_dict"] # "model_args" contain additional info
 # Remove "actor_critic." from keys to align names
 weights = {k[len('actor_critic.') :]: v for k, v in weight_dict.items()}
@@ -40,7 +43,7 @@ actor_critic.load_state_dict(weights)
 
 # , self.goal_sensor_uuid,
 
-depth_observation = torch.zeros(256,256,1).unsqueeze(0)
+depth_observation = torch.rand(256,256,1).unsqueeze(0)
 target_observation = torch.FloatTensor([1,0]).unsqueeze(0)
 observations = {
     "depth": depth_observation,
@@ -57,6 +60,7 @@ rnn_hidden_states = torch.zeros(actor_critic.net.num_recurrent_layers,
 #print("unpacked: " + str(actor_critic.net.state_encoder._unpack_hidden(rnn_hidden_states)[1].shape))
 masks = torch.zeros(1,1)
 
+# actions: [stop, forward (0.25m), left (10deg), right (10deg)]
 value, action, action_log_probs, rnn_hidden_states = actor_critic.act(
              observations,
              rnn_hidden_states,
