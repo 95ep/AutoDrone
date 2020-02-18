ecfbcad904f31a9ba97dc9b85aa0008e0cf9ed00
Filip <filhei@student.chalmers.se>
2020-02-18 14:07:42 +0100

    Added progress bar function

--- PointNavigation/trainer_new.py
+++ PointNavigation/trainer_new.py
@@ -14,6 +14,30 @@ from cv2 import resize, INTER_LINEAR
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
+def print_progress_bar (iteration, total, prefix = '', suffix = '',
+                        decimals = 1, length = 50, fill = '=', print_end = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str) █
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filled_length = int(length * iteration // total)
+    bar = fill * filled_length + ' ' * (length - filled_length)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end=print_end)
+    # Print New Line on Complete
+    if iteration == total:
+        time.sleep(0.05)
+        print('\r ' + ' ' * (filled_length + 10), end=print_end)
+
+
 def process_obs(obs_from_env, env_str, param):
     obs_vector = None
     obs_visual = None
