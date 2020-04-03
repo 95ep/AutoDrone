25f562a02f195bfc1c9b8cdac8eeb71fafa5eddd
95ep <erik.ao.persson@gmail.com>
2020-04-03 16:00:20 +0200

    Updates of metric plotter

--- MetricsPlotters/metrics.py
+++ MetricsPlotters/metrics.py
@@ -1,34 +1,34 @@
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
-crashes_path = "D:/Exjobb2020ErikFilip/AutoDrone/runs/Downloaded_metrics/basic23-invalid-trgts-continued/Eval_nDones.csv"
-terminations_correct_path = "D:/Exjobb2020ErikFilip/AutoDrone/runs/Downloaded_metrics/basic23-invalid-trgts-continued/Eval_nTerminationsCorrect.csv"
-terminations_incorrect_path = "D:/Exjobb2020ErikFilip/AutoDrone/runs/Downloaded_metrics/basic23-invalid-trgts-continued/Eval_nTerminationsIncorrect.csv"
-total_return_path = "D:/Exjobb2020ErikFilip/AutoDrone/runs/Downloaded_metrics/basic23-invalid-trgts-continued/Eval_TotalReturn.csv"
 
-metric_labels = ["# Crashes", "# Terminations Correct", "# Terminations Incorrect", "Total return"]
-metrics = []
-df = pd.read_csv(crashes_path)
+# Edit these variables
+save_dir = "pong"
+metric_path = "/Users/erikpersson/PycharmProjects/AutoDrone/MetricsPlotters/plots/pong/total_return.csv"
+fig_name = "total_return"
+fig_title = ""
+x_label = "Epoch"
+y_label = "Total return"
+label_fontsize = 16
+tick_fontsize = 12
+dpi = 200
+
+
+
+parent_dir = "plots/" + save_dir
+if not os.path.exists(parent_dir):
+    os.makedirs(parent_dir)
+
+df = pd.read_csv(metric_path)
 epochs = df.Step
-metrics.append(df.Value)
+metric = df.Value
 
-df = pd.read_csv(terminations_correct_path)
-metrics.append(df.Value)
-
-df = pd.read_csv(terminations_incorrect_path)
-metrics.append(df.Value)
-
-df = pd.read_csv(total_return_path)
-metrics.append(df.Value)
-
-
-
-for i, metric in enumerate(metrics):
-    label = metric_labels[i]
-    fig = plt.figure()
-    ax = fig.subplots()
-    ax.plot(epochs, metric)
-    ax.set_xlabel("Epoch")
-    ax.set_title(label)
-    fig.savefig(label + ".png")
+fig = plt.figure()
+ax = fig.subplots()
+ax.plot(epochs, metric)
+ax.set_xlabel(x_label, fontsize=label_fontsize)
+ax.set_ylabel(y_label, fontsize=label_fontsize)
+ax.set_title(fig_title)
+ax.tick_params(axis='both', labelsize=tick_fontsize)
+fig.savefig("plots/" + save_dir + "/" + fig_name, dpi=dpi)
