99be25bf06f7d06a7cf1d8393163caaa48d04598
Erik Persson <erik.ao.persson@gmail.com>
2020-04-17 16:04:51 +0200

    Fixed losing altitude after move up or forward

--- Environments/airgym/agent_controller.py
+++ Environments/airgym/agent_controller.py
@@ -19,20 +19,23 @@ def rotate_right(client, duration=0.5, rate=20):
 
 
 def move_forward(client):
+    # AirSim is buggy and looses alt if z-vel is 0
+    keep_altitude_z_vel = -3e-3
     yaw = get_orientation(client)
+
     # Calc velocity vector of magnitude 0.5 in direction of UAV
     vel = (0.5*np.cos(yaw), 0.5*np.sin(yaw), 0)
     # Move forward approx 0.25 meter
     client.moveByVelocityAsync(vel[0], vel[1], vel[2], duration=0.5).join()
-    # Stop the UAV
-    client.moveByVelocityAsync(0, 0, 0, duration=1e-3).join()
+    # Stop the UAV, small z-vel to keep altitude
+    client.moveByVelocityAsync(0, 0, -3e-3, duration=1e-6).join()
 
 
 def move_up(client, velocity=0.5, duration=0.5):
     # Move up approx 0.25 m. Note direction of z-axis.
     client.moveByVelocityAsync(0, 0, -velocity, duration=duration).join()
     # Stop the UAV
-    client.moveByVelocityAsync(0, 0, 0, duration=1e-3).join()
+    client.rotateByYawRateAsync(0, 1e-6).join()
 
 
 def move_down(client, velocity=0.5, duration=0.5):
