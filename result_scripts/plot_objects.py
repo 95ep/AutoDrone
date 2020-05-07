021872a395307ff50a6d37cb8bf394377d1a1bf0
95ep <erik.ao.persson@gmail.com>
2020-05-07 16:06:31 +0200

    Updated scripts for plotting verification

--- result_scripts/plot_objects.py
+++ result_scripts/plot_objects.py
@@ -1,11 +1,12 @@
 import numpy as np
 import vtkplotter as vtkp
 
-
-gt_pos = np.load('/Users/erikpersson/PycharmProjects/AutoDrone/verification/test/ground_truth.npy')
-obstac = np.load('/Users/erikpersson/PycharmProjects/AutoDrone/verification/test/obstacles.npy')
-objs = np.load('/Users/erikpersson/PycharmProjects/AutoDrone/verification/test/objects.npy')
-visited = np.load('/Users/erikpersson/PycharmProjects/AutoDrone/verification/test/visited.npy')
+save_pth = '/Users/erikpersson/PycharmProjects/AutoDrone/plots/objs_ver2'
+gt_pos = np.load('/Users/erikpersson/PycharmProjects/AutoDrone/verification/local3_epoch_2270_objs/ground_truth.npy')
+obstac = np.load('/Users/erikpersson/PycharmProjects/AutoDrone/verification/local3_epoch_2270_objs/obstacles.npy')
+objs = np.load('/Users/erikpersson/PycharmProjects/AutoDrone/verification/local3_epoch_2270_objs/objects.npy')
+visited = np.load('/Users/erikpersson/PycharmProjects/AutoDrone/verification/local3_epoch_2270_objs/visited.npy')
+ceiling_level = -1.2
 
 print("Shape of gt_pos {}".format(gt_pos.shape))
 print("Shape of obstac {}".format(obstac.shape))
@@ -13,17 +14,61 @@ print("Shape of objs {}".format(objs.shape))
 print("Shape of visited {}".format(visited.shape))
 
 gt_color = [0, 0, 255, 150]
-obj_color = [255, 128, 0, 255]
-visited_color = [0, 255, 0, 80]
+obj_color = [255, 255, 0, 255]
+obstac_color = [255, 51, 51, 140]
+visited_color = [0, 255, 0, 70]
 
-plist = np.concatenate((visited, gt_pos, objs), axis=0)
-c = []
+obstac_list = []
+for o in obstac:
+    if o[0] < 25 and o[1] < 10 and o[2] > ceiling_level:
+        obstac_list.append(o)
+obstac = np.array(obstac_list)
+
+visited_list = []
+for v in visited:
+    if v[0] < 25 and v[1] < 10:
+        visited_list.append(v)
+visited = np.array(visited_list)
+
+plist1 = np.concatenate((visited, gt_pos, objs), axis=0)
+c1 = []
 for _ in range(visited.shape[0]):
-    c.append(visited_color)
+    c1.append(visited_color)
 for _ in range(gt_pos.shape[0]):
-    c.append(gt_color)
+    c1.append(gt_color)
 for _ in range(objs.shape[0]):
-    c.append(obj_color)
+    c1.append(obj_color)
 
-points = vtkp.shapes.Points(plist, r=20, c=c)
-vtkp.show(points, axes=8)
+c2 = c1.copy()
+for _ in range(obstac.shape[0]):
+    # c2.insert(0, obstac_color)
+    c2.append(obstac_color)
+
+# plist2 = np.concatenate((obstac, plist1), axis=0)
+plist2 = np.concatenate((plist1, obstac), axis=0)
+
+
+points1 = vtkp.shapes.Points(plist1, r=20, c=c1)
+points2 = vtkp.shapes.Points(plist2, r=20, c=c2)
+
+axes_dict = dict(
+    xyGrid=False,
+    yzGrid=False,
+    showTicks=True,
+    xLabelSize=0,
+    yLabelSize=0,
+    zLabelSize=0,
+    xtitle=' ',
+    ytitle=' ',
+    ztitle=' ',
+    axesLineWidth=3
+)
+vp = vtkp.Plotter(axes=axes_dict, interactive=False, size=(10000,10000))
+vp += points1
+vp.show(azimuth=70, elevation=165,roll=-90)
+vtkp.screenshot(save_pth+'.png')
+
+vp = vtkp.Plotter(axes=axes_dict, interactive=False, size=(10000,10000))
+vp += points2
+vp.show(azimuth=70, elevation=165,roll=-90)
+vtkp.screenshot(save_pth+'_with_obstac.png')
--- result_scripts/plot_obstacles.py
+++ result_scripts/plot_obstacles.py
@@ -2,14 +2,16 @@ import numpy as np
 import vtkplotter as vtkp
 from matplotlib.colors import ListedColormap
 
-ceiling_level = -1.91
+ceiling_level = -2.0
+# ceiling_level = None
 
-gt_pos = np.load('/Users/erikpersson/PycharmProjects/AutoDrone/verification/test/ground_truth.npy')
-obstac = np.load('/Users/erikpersson/PycharmProjects/AutoDrone/verification/test/obstacles.npy')
-objs = np.load('/Users/erikpersson/PycharmProjects/AutoDrone/verification/test/objects.npy')
-visited = np.load('/Users/erikpersson/PycharmProjects/AutoDrone/verification/test/visited.npy')
+save_pth = '/Users/erikpersson/PycharmProjects/AutoDrone/plots/obstac_ver_with_visited.png'
+gt_pos = np.load('/Users/erikpersson/PycharmProjects/AutoDrone/verification/local3_epoch_2270_obstac/ground_truth.npy')
+obstac = np.load('/Users/erikpersson/PycharmProjects/AutoDrone/verification/local3_epoch_2270_obstac/obstacles.npy')
+objs = np.load('/Users/erikpersson/PycharmProjects/AutoDrone/verification/local3_epoch_2270_obstac/objects.npy')
+visited = np.load('/Users/erikpersson/PycharmProjects/AutoDrone/verification/local3_epoch_2270_obstac/visited.npy')
 
-cell_scale = np.array([0.5, 0.5, 0.5])
+cell_scale = np.array([0.71, 0.71, 0.71])
 
 print("Shape of gt_pos {}".format(gt_pos.shape))
 print("Shape of obstac {}".format(obstac.shape))
@@ -23,6 +25,11 @@ grid_shape = ((max_vals - min_vals) // cell_scale + 1).astype(int)
 print(grid_shape)
 grid = np.zeros(grid_shape)
 
+# Add visited
+for i in range(visited.shape[0]):
+    pos = visited[i, :]
+    idx = ((pos - min_vals) // cell_scale).astype(int)
+    grid[idx[0], idx[1], idx[2]] = 3
 
 # Add obstacles
 for i in range(obstac.shape[0]):
@@ -33,7 +40,7 @@ for i in range(obstac.shape[0]):
     idx = ((pos - min_vals) // cell_scale).astype(int)
     grid[idx[0], idx[1], idx[2]] = 4
 
-factor = 2
+factor = 4
 new_grid = np.zeros((grid_shape[0]*factor, grid_shape[1]*factor, grid_shape[2]*factor))
 for i in range(factor):
     for j in range(factor):
@@ -41,17 +48,11 @@ for i in range(factor):
             new_grid[i::factor, j::factor, k::factor] = grid
 grid = new_grid
 
-print(grid.shape)
-print(np.amax(grid))
-print(np.amin(grid))
-color_list = np.array([[0., 0., 0.3, 0.1],  # unknown starts from 1
-                       [0.9, 0.95, 1., 0.2],  # visible
-                       [0.2, 1., 0.2, 0.8],  # visited
-                       [1., 0.2, 0.2, 0.6],  # obstacle
-                       [1., 1., 0, 1.],  # object
-                       [0., 0.7, 0., 1.]])  # position
+color_list = np.array([[0.9, 0.95, 1., 0.2], [0.2, 1., 0.2, 0.8], [1., 0.0, 0.0, 0.1], [1., 1., 0, 1.], [0., 0.7, 0., 1.]])
+
 newcmp = ListedColormap(color_list)
 
 vol = vtkp.Volume(grid)
-lego = vol.legosurface(vmin=2, vmax=6)
-vtkp.show(lego, axes=8)
+lego = vol.legosurface(vmin=2, vmax=6, cmap=newcmp)
+vtkp.show(lego, axes=0, azimuth=70, elevation=165,roll=-90, interactive=False)
+vtkp.screenshot(save_pth)
