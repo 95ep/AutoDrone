b4bc52291b077fa40358c64676cda805397e669b
Erik Persson <erik.ao.persson@gmail.com>
2020-05-08 15:35:08 +0200

    Updated plotter of training metrics

--- MetricsPlotters/metrics.py
+++ MetricsPlotters/metrics.py
@@ -7,46 +7,43 @@ import matplotlib.pyplot as plt
 label_fontsize = 16
 tick_fontsize = 12
 dpi = 200
-save_dir = "pong"
+save_dir = "local_nav"
 gamma = 0.6
 color_alpha = 0.3
 
+if True:
+    # Plot 1
+    #change here
+    train_return_path = "D:/Exjobb2020ErikFilip/AutoDrone/MetricsPlotters/plots/local_nav/nTerminationsIncorrect.csv"
+    human_baseline = 1.33
+    fig_name = "n_terminations_incorrect"
+    fig_title = ""
+    x_label = "Epoch"
+    y_label = "# terminations incorrect"
+    ylim = [0, 130]
 
-# Plot 1
-train_return_path = "/Users/erikpersson/PycharmProjects/AutoDrone/MetricsPlotters/plots/pong/total_return.csv"
-eval_return_path = "/Users/erikpersson/PycharmProjects/AutoDrone/MetricsPlotters/plots/pong/total_return.csv"
-fig_name = "total_return"
-fig_title = ""
-x_label = "Epoch"
-y_label = "Total return"
-human_baseline = 152
+    parent_dir = "plots/" + save_dir
+    if not os.path.exists(parent_dir):
+        os.makedirs(parent_dir)
 
-parent_dir = "plots/" + save_dir
-if not os.path.exists(parent_dir):
-    os.makedirs(parent_dir)
+    df = pd.read_csv(train_return_path)
+    epochs_train = df.Step
+    tot_ret_train = df.Value
 
-df = pd.read_csv(train_return_path)
-epochs_train = df.Step
-tot_ret_train = df.Value
+    tot_ret_train_ema = tot_ret_train.ewm(alpha=1-gamma, adjust=True).mean()
 
-df = pd.read_csv(eval_return_path)
-epochs_eval = df.Step
-tot_ret_eval = df.Value
+    fig = plt.figure()
+    ax = fig.subplots()
+    ax.set_ylim(ylim[0], ylim[1])
+    ax.plot(epochs_train, tot_ret_train, 'r-', label='_nolegend_', alpha=color_alpha)
+    ax.plot(epochs_train, tot_ret_train_ema, 'r-', label='Training')
+    ax.plot([epochs_train.iloc[0], epochs_train.iloc[-1]], [human_baseline, human_baseline], 'b-.', label='Human baseline')
+    ax.set_xlabel(x_label, fontsize=label_fontsize)
+    ax.set_ylabel(y_label, fontsize=label_fontsize)
+    ax.set_title(fig_title)
+    ax.tick_params(axis='both', labelsize=tick_fontsize)
+    ax.legend(fontsize=tick_fontsize)
 
-tot_ret_train_ema = tot_ret_train.ewm(alpha=1-gamma, adjust=True).mean()
-tot_ret_eval_ema = (tot_ret_eval-20).ewm(alpha=1-gamma, adjust=True).mean()
-
-fig = plt.figure()
-ax = fig.subplots()
-ax.plot(epochs_train, tot_ret_train, 'r-', label='_nolegend_', alpha=color_alpha)
-ax.plot(epochs_train, tot_ret_train_ema, 'r-', label='Training')
-ax.plot(epochs_eval, tot_ret_eval-20, 'g--', label='_nolegend', alpha=color_alpha)
-ax.plot(epochs_eval, tot_ret_eval_ema, 'g--', label='Evaluation')
-ax.plot([epochs_train.iloc[0], epochs_train.iloc[-1]], [human_baseline, human_baseline], 'b-.', label='Human baseline')
-ax.set_xlabel(x_label, fontsize=label_fontsize)
-ax.set_ylabel(y_label, fontsize=label_fontsize)
-ax.set_title(fig_title)
-ax.tick_params(axis='both', labelsize=tick_fontsize)
-ax.legend(fontsize=tick_fontsize)
-fig.savefig("plots/" + save_dir + "/" + fig_name, dpi=dpi)
-plt.show()
+    strFile = "plots/" + save_dir + "/" + fig_name
+    fig.savefig(strFile, dpi=dpi, bbox_inches='tight')
+    plt.show()
